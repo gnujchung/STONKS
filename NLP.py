c7808d2ce4ca9d95c5f581c0e207b17ef46c949b
anthony-innamorato <ai974@nyu.edu>
2020-03-03 10:39:22 -0500

    Working search

--- NLP.py
+++ NLP.py
@@ -1,5 +1,16 @@
 import twitter
 
+def buildTestSet(search_keyword):
+    try:
+        tweets_fetched = twittApi.GetSearch(search_keyword, count = 100)
+
+        print("Fetched " + str(len(tweets_fetched)) + " tweets for the term " + search_keyword)
+
+        return [{"text":status.text, "label":None} for status in tweets_fetched]
+    except:
+        print("Unfortunately, something went wrong..")
+        return None
+
 twitterKeys = open("API.txt", 'r').read().splitlines()[1:]
 
 twittApi = twitter.Api(consumer_key=twitterKeys[0],
@@ -7,4 +18,7 @@ twittApi = twitter.Api(consumer_key=twitterKeys[0],
                         access_token_key=twitterKeys[2],
                         access_token_secret=twitterKeys[3])
 
-print(twittApi.VerifyCredentials())
+search_term = input("Enter a search keyword:")
+testDataSet = buildTestSet(search_term)
+
+print(testDataSet[0:4])
