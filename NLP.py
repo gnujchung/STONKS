e4a13d9372bb77a1835fa49d00c3183dafc91d89
anthony-innamorato <ai974@nyu.edu>
2020-03-04 12:18:52 -0500

    Building training set functioknality complete

--- NLP.py
+++ NLP.py
@@ -1,16 +1,13 @@
 import twitter
+import csv
+import time
+import sys
 
-def buildTestSet(search_keyword):
-    try:
-        tweets_fetched = twittApi.GetSearch(search_keyword, count = 100)
+#test set should be len 100 when actually running model
+    #set to 10 for testing to not reach 180 tweets/15 min API allows
+TEST_SET_SIZE = 10
 
-        print("Fetched " + str(len(tweets_fetched)) + " tweets for the term " + search_keyword)
-
-        return [{"text":status.text, "label":None} for status in tweets_fetched]
-    except:
-        print("Unfortunately, something went wrong..")
-        return None
-
+#open API keys (omit first line as it contains format)
 twitterKeys = open("API.txt", 'r').read().splitlines()[1:]
 
 twittApi = twitter.Api(consumer_key=twitterKeys[0],
@@ -18,7 +15,69 @@ twittApi = twitter.Api(consumer_key=twitterKeys[0],
                         access_token_key=twitterKeys[2],
                         access_token_secret=twitterKeys[3])
 
-search_term = input("Enter a search keyword:")
-testDataSet = buildTestSet(search_term)
+############################### Build Test Set #################################
 
-print(testDataSet[0:4])
+def buildTestSet(searchWord, testSetSize):
+    print("Beginning Search with:", searchWord, "size:", testSetSize)
+    try:
+        tweets = twittApi.GetSearch(searchWord, count=testSetSize)
+        print("Found:", len(tweets), "tweets for:", searchWord)
+        out = [{"text":status.text, "label":None} for status in tweets]
+
+        #UnicodeEncodeError can occur (define error behavior as replace with ?)
+        sysEncoding = sys.stdout.encoding
+        for i in range(len(out)):
+            out[i]["text"] = out[i]["text"].encode(sysEncoding, errors="replace")
+        return out
+    except Exception as e:
+        print("Exception raised:", e)
+        return None
+
+testDataSet = buildTestSet(input("Enter a search keyword:"), TEST_SET_SIZE)
+if testDataSet == None:
+    sys.exit("ABORT: error raised in testSet")
+
+print("testDataSet:", testDataSet)
+
+############################## Build Training Set ##############################
+
+def buildTrainingSet(corpusFile, tweetDataFile):
+    corpus = []
+
+    with open(corpusFile,'r') as csvfile:
+        lineReader = csv.reader(csvfile,delimiter=',', quotechar="\"")
+        for row in lineReader:
+            corpus.append({"tweet_id":row[2], "label":row[1], "topic":row[0]})
+
+    rate_limit = 180
+    sleep_time = 900/180
+
+    trainingDataSet = []
+
+    for tweet in corpus:
+        try:
+            status = twittApi.GetStatus(tweet["tweet_id"])
+            print("Tweet fetched:", status.text)
+            tweet["text"] = status.text
+            trainingDataSet.append(tweet)
+            time.sleep(sleep_time)
+        except Exception as e:
+            print("Exception raised:", e)
+            continue
+
+    # now we write them to the empty CSV file
+    with open(tweetDataFile,'w') as csvfile:
+        linewriter = csv.writer(csvfile,delimiter=',',quotechar="\"")
+        for tweet in trainingDataSet:
+            try:
+                linewriter.writerow([tweet["tweet_id"], tweet["text"],
+                    tweet["label"], tweet["topic"]])
+            except Exception as e:
+                print(e)
+    return trainingDataSet
+
+print("\n\n================BUILDING TRAINING SET================\n\n")
+trainingData = buildTrainingSet("corpus.csv", "tweetDataFile.csv")
+print("training data built")
+
+################################################################################
