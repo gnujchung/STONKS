a65d658e4e13f2f3610ebb7cb87455d4efad261a
anthony-innamorato <ai974@nyu.edu>
2020-03-15 19:35:30 -0400

    added elapsed time, pruned irrelevant data, search for DOW 30, removed extra dict keys, cleaned up ode

--- NLP.py
+++ NLP.py
@@ -8,28 +8,39 @@ import string
 from nltk.corpus import stopwords
 import json
 import nltk
+from time import time
 
 '''
 TODO:
+    add elapsed time
+    cut words w less than 3 char out
+    check all funcs
+    refine search to pull from stock accts
     find more data (too neutral for now)
         - added happy.txt and sad.txt --- not enough
-    clean up code
+        - added stocksDataSet.csv --DONE
+    prune all "irrelevant" data from original dataset
+        -DONE
     search for all 30 in DOW
         -DONE
-    refine search to pull from stock accts
+    remove extra dict keys
+        -DONE
+    clean up code
+        -DONE
 '''
 
-#test set should be at least len 100 when actually running model
-    #set to 10 for testing to not reach 180 tweets/15 min API allows
-TEST_SET_SIZE = 180
-EXCEL_CREATED = True
+TEST_SET_SIZE = 300 # total number of tweets fetched for the testing data
+EXCEL_CREATED = True #tweetDataFile.csv was built by manually requesting the tweets
 KEYWORDS = [
     "AAPL", "AXP", "BA", "CAT", "CSCO", "CVX", "DIS", "DOW", "GS", "HD", "IBM",
     "INTC", "JNJ", "JPM", "KO", "MCD", "MMM", "MRK", "MSFT", "NKE", "PFE", "PG",
     "TRV", "UNH", "UTX", "V", "VZ", "WBA", "WMT", "XOM"
 ]
 
-#open API keys (omit first line as it contains format)
+currTime = time()
+
+# open API keys (omit first line as it contains format)
+# API keys should NOT be stored in github repo, request keys from Tony if needed
 twitterKeys = open("API.txt", 'r').read().splitlines()[1:]
 
 twittApi = twitter.Api(consumer_key=twitterKeys[0],
@@ -53,14 +64,15 @@ def buildTestSet(searchWord, testSetSize):
         return out
     except Exception as e:
         print("Exception raised:", e)
-        return None
+        sys.exit("ABORT: Twitter API error for:", searchWord)
+
 testDataSet = []
 for key in KEYWORDS:
     testDataSet.append(buildTestSet(key, TEST_SET_SIZE//len(KEYWORDS)))
-if testDataSet == []:
-    sys.exit("ABORT: testSet empty")
-
-print("testDataSet complete")
+newTime = time()
+elapsed = round(newTime - currTime, 2)
+currTime = newTime
+print("testDataSet complete in", elapsed, "seconds")
 
 ############################## Build Training Set ##############################
 
@@ -74,10 +86,8 @@ def buildOrigTrainingSet(corpusFile, tweetDataFile):
             for row in lineReader:
                 if row != []:
                     objJson = {}
-                    objJson["tweet_id"] = row[0]
-                    objJson["text"] = row[1]
-                    objJson["label"] = row[2]
-                    objJson["topic"] = row[3]
+                    objJson["text"] = row[0]
+                    objJson["label"] = row[1]
                     trainingDataSet.append(objJson)
         return trainingDataSet
 
@@ -86,7 +96,7 @@ def buildOrigTrainingSet(corpusFile, tweetDataFile):
     with open(corpusFile,'r') as csvfile:
         lineReader = csv.reader(csvfile,delimiter=',', quotechar="\"")
         for row in lineReader:
-            corpus.append({"tweet_id":row[2], "label":row[1], "topic":row[0]})
+            corpus.append({"tweet_id":row[2], "label":row[1]})
 
     rate_limit = 180
     sleep_time = 900/rate_limit
@@ -96,6 +106,7 @@ def buildOrigTrainingSet(corpusFile, tweetDataFile):
             status = twittApi.GetStatus(tweet["tweet_id"])
             print("Tweet fetched:", status.text)
             tweet["text"] = status.text
+            del tweet["tweet_id"]
             trainingDataSet.append(tweet)
             time.sleep(sleep_time)
         except Exception as e:
@@ -107,27 +118,38 @@ def buildOrigTrainingSet(corpusFile, tweetDataFile):
         linewriter = csv.writer(csvfile,delimiter=',',quotechar="\"")
         for tweet in trainingDataSet:
             try:
-                linewriter.writerow([tweet["tweet_id"], tweet["text"],
-                    tweet["label"], tweet["topic"]])
+                linewriter.writerow([tweet["text"], tweet["label"]])
             except Exception as e:
                 print(e)
     return trainingDataSet
 
-def altTrainingSet(goodFile, badFile):
-    def returnDict(line, sentiment):
-        return {"tweet_id": "", "text": line.strip(), "label": sentiment, "topic": ""}
+def dualTrainingSet(goodFile, badFile):
     resLst = []
     goodFile = open(goodFile, 'r')
     for line in goodFile:
-        resLst.append(returnDict(line, 'positive'))
+        resLst.append({"text": line.strip(), "label": "positive"})
+    badFile = open(badFile, 'r')
     for line in badFile:
-        resLst.append(returnDict(line, 'negative'))
+        resLst.append({"text": line.strip(), "label": "negative"})
     return resLst
+
+def stocksTrainingSet(fileName):
+    resLst = []
+    stocksFile = open(fileName, 'r')
+    for line in stocksFile:
+        cols = line.strip().split(',')
+        resLst.append({"text": cols[1], "label": cols[2]})
+    return resLst
+
 
 print("\n\n----------------BUILDING TRAINING SET----------------\n\n")
 trainingData = buildOrigTrainingSet("corpus.csv", "tweetDataFile.csv")
-trainingData += altTrainingSet("happy.txt", "sad.txt")
-print("training data built")
+trainingData += dualTrainingSet("happy.txt", "sad.txt")
+#trainingData += stocksTrainingSet("stocksDataSet.csv")
+newTime = time()
+elapsed = round(newTime - currTime, 2)
+currTime = newTime
+print("training data built in:", elapsed, "seconds")
 
 ################################################################################
 class PreProcessTweets:
@@ -156,7 +178,10 @@ preprocessedTestSet = []
 for keySet in testDataSet:
     preprocessedTestSet.append(tweetProcessor.processTweets(keySet))
 
-print("processing complete")
+newTime = time()
+elapsed = round(newTime - currTime, 2)
+currTime = newTime
+print("processing complete in:", elapsed, "seconds")
 
 ################################################################################
 def buildVocabulary(preprocessedTrainingData):
@@ -177,12 +202,21 @@ def extract_features(tweet):
         features['contains(%s)' % word] = (word in tweet_words)
     return features
 
-print("\n\n----------------RUNNING MODEL----------------\n\n")
+print("\n\n----------------TRAINING MODEL----------------\n\n")
 
 word_features = buildVocabulary(preprocessedTrainingSet)
 trainingFeatures = nltk.classify.apply_features(extract_features, preprocessedTrainingSet)
 
+print("training")
 NBayesClassifier = nltk.NaiveBayesClassifier.train(trainingFeatures)
+newTime = time()
+elapsed = round(newTime - currTime, 2)
+currTime = newTime
+print("training complete in:", elapsed, "seconds")
+
+################################################################################
+
+print("\n\n----------------RUNNING MODEL----------------\n\n")
 
 for i in range(len(preprocessedTestSet)):
     eachStock = preprocessedTestSet[i]
@@ -193,10 +227,14 @@ for i in range(len(preprocessedTestSet)):
 
     # get the majority vote
     if posRes == negRes:
-        print("\tOverall Neutral Sentiment.", posRes, "out of:", TEST_SET_SIZE//len(KEYWORDS))
+        print("\tOverall Neutral Sentiment.", posRes+negRes, "out of:", len(eachStock))
     elif posRes > negRes:
         print("\tOverall Positive Sentiment")
         print("\t\tPositive Sentiment Percentage = " + str(100*posRes/len(NBResultLabels)) + "%")
     else:
         print("\tOverall Negative Sentiment")
         print("\t\tNegative Sentiment Percentage = " + str(100*negRes/len(NBResultLabels)) + "%")
+newTime = time()
+elapsed = round(newTime - currTime, 2)
+currTime = newTime
+print("running complete in:", elapsed, "seconds")
