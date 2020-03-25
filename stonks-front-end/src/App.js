9c29ccd188695dea7f6cb57eada1bb3d059caec3
Simone Brown <smb866@nyu.edu>
2020-03-25 12:38:50 -0400

    adding front end files

---
+++ stonks-front-end/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import EnhancedTable from './Table';
+
+function App() {
+  return (
+    <div className="App">
+      <EnhancedTable />
+
+    </div>
+  );
+}
+
+export default App;
+
